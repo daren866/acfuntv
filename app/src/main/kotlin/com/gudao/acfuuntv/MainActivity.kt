68e758f55a256e10c8b09c2ed37ad8141a175447
daren866 <xiao1xiao188@qq.com>
2026-07-10 21:01:32 +0800

    feat: 恢复完整的TV主页UI

--- app/src/main/kotlin/com/gudao/acfuuntv/MainActivity.kt
+++ app/src/main/kotlin/com/gudao/acfuuntv/MainActivity.kt
@@ -3,37 +3,19 @@ package com.gudao.acfuuntv
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
+import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.gudao.acfuuntv.ui.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            TestScreen()
-        }
-    }
-}
-
-@Composable
-fun TestScreen() {
-    MaterialTheme {
-        androidx.compose.foundation.layout.Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Black)
-        ) {
-            Text(
-                text = "ACFun TV Test",
-                color = Color(0xFFFF2C55),
-                fontSize = 40.sp
-            )
+            AppTheme {
+                MainActivityContent()
+            }
         }
     }
 }
